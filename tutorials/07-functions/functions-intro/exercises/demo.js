67439821cf7c1b1d5c85312c7087d76f67bf6fb9
holly <hello.hollyeve@outlook.com>
2026-09-05 19:21:02 -0400

    complete

--- tutorials/07-functions/functions-intro/exercises/demo.js
+++ tutorials/07-functions/functions-intro/exercises/demo.js
@@ -3,17 +3,27 @@
 /** EXAMPLE 1 - DECLARING & CALLING **/
 
 // DEMO: Declare a function that prints a report when solar panels are deployed
+function deploySolarPanels() {
+    console.log("Unlatching hinges");
+    console.log("Solar panels fully deployed.");
+}
 
 // DEMO: Call the function twice in a row to see it being used and reused
+deploySolarPanels();
+deploySolarPanels();
 
 /** EXAMPLE 2 - CODE ORGANIZATION **/
 
 // DEMO: Declare a function that prints an emergency message
+function soundEmergencySiren () {
+    console.log("ALERT: HULL BREACH DETECTED")
+}
 
 let hullBreach = true;
 
 if (hullBreach) {
 	// DEMO: Call the function to sound the siren
+    soundEmergencySiren();
 }
 
 // DEMO: Make a git commit!
--- tutorials/07-functions/functions-intro/exercises/practice.js
+++ tutorials/07-functions/functions-intro/exercises/practice.js
@@ -13,6 +13,12 @@
     Then, invoke the function once below its declaration.
 */
 
+function greetCrew () {
+    console.log("Welcome back, Captain.");
+}
+
+greetCrew ();
+
 /** EXERCISE 2: LIFE SUPPORT BOOST **/
 
 /*
@@ -20,6 +26,10 @@
     Inside, log "Pumping extra oxygen into sectors A-D... Done."
     Do not call it yet.
 */
+
+function activateOxygenBoost () {
+    console.log("Pumping extra oxygen into sectors A-D... Done.");
+}
 
 /** EXERCISE 3: TRIGGERING THE PROTOCOL **/
 
@@ -30,6 +40,10 @@ let crewIsSleepy = true;
     is true. If it is, invoke the 'activateOxygenBoost' function 
     you created in Exercise 2.
 */
+
+if (crewIsSleepy) { 
+    activateOxygenBoost();
+}
 
 /** EXERCISE 4: SEQUENTIAL AUTOMATION **/
 
@@ -45,3 +59,6 @@ function clearLaunchPad() {
     TODO: Call 'clearLaunchPad' first, and then call 'startEngines' 
     immediately after to ensure a safe, sequential liftoff.
 */
+
+clearLaunchPad();
+startEngines();
