1547a1bb17f2daa8a897faf5f0379d67cf031c8d
holly <hello.hollyeve@outlook.com>
2026-09-06 13:26:47 -0400

    completed demo.js

--- tutorials/07-functions/variable-scope/exercises/demo.js
+++ tutorials/07-functions/variable-scope/exercises/demo.js
@@ -9,6 +9,7 @@ function displayShipName() {
 }
 
 // DEMO: Call the function above to see if the shipName variable is in scope
+displayShipName(); //anything defined globally is accessible anywhere
 
 /** EXAMPLE 2 - LOCAL SCOPE (Parameters & Functions) **/
 
@@ -22,10 +23,13 @@ function processDriveCoolant(incomingWaterTons) {
 }
 
 // DEMO: Call the function above
+processDriveCoolant(100);
 
 // DEMO: Log the parameter incomingWaterTons on the line below. What happens?
+//console.log(incomingWaterTons); //ref error
 
 // DEMO: Log the variable conversionFactor on the line below. What happens?
+//console.log(conversionFactor);  //ref error
 
 /** EXAMPLE 3 - BLOCK SCOPE (Loops & Conditions) **/
 
@@ -34,10 +38,11 @@ let fuelPods = [20, 40, 10];
 for (let i = 0; i < fuelPods.length; i++) {
 	let temporaryCalculation = fuelPods[i] * 1.1;
 	// DEMO: Un-comment this line to see what is happening when the loop runs
-	// console.log(`Pod adjusted weight: ${temporaryCalculation} t`);
+	console.log(`Pod adjusted weight: ${temporaryCalculation} t`);
 }
 
 // DEMO: Log the loop variable i to the console on the line below. What happens?
+//console.log(i); //cannot be accessed outside of the block
 
 // DEMO: Log temporaryCalculation to the console on the line below. What happens?
 
